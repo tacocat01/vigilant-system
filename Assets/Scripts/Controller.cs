51e8ca7802f11bad2f7476cc6d5d5e2747bfb053
Christopher <41389677+Zenif-NIght@users.noreply.github.com>
2022-03-19 04:22:42 -0600

    controller stuff

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Controller : MonoBehaviour
-{
+{   
+    // speed of the master object
+    public float speed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,20 +15,55 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+
+        // USE WASD TO MOVE THE MASTER OBJECT
+        // IF USER  presses w key, move the master object forward
+        if (Input.GetKey(KeyCode.W))
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+            // move the master object forward
+            transform.position = transform.position + transform.forward * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            // move the master object backward
+            transform.position = transform.position - transform.forward * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            // move the master rotation by one degree
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y - 1, transform.rotation.eulerAngles.z);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            // move the master rotation by one degree
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + 1, transform.rotation.eulerAngles.z);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            // move the master rotation by one degree
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x - 1, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            // move the master rotation by one degree
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x + 1, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.rotation = Quaternion.Euler(-1 * Time.deltaTime, 0, 0);
-            //transform.rotate(1 * Time.deltaTime, 0, 0);
-        }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.rotation = Quaternion.Euler(1 * Time.deltaTime, 0, 0);
-        }
+        // if (Input.GetKey(KeyCode.UpArrow))
+        // {
+        //     transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+        // }
+
+        // if (Input.GetKey(KeyCode.RightArrow))
+        // {
+        //     transform.rotation = Quaternion.Euler(-1 * Time.deltaTime, 0, 0);
+        //     //transform.rotate(1 * Time.deltaTime, 0, 0);
+        // }
+
+        // if (Input.GetKey(KeyCode.LeftArrow))
+        // {
+        //     transform.rotation = Quaternion.Euler(1 * Time.deltaTime, 0, 0);
+        // }
     }
 }
