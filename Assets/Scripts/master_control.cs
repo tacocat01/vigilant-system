b3885b84ce775af3901172d005606fd5f6653bda
Christopher <41389677+Zenif-NIght@users.noreply.github.com>
2022-03-19 04:27:20 -0600

    Update master_control.cs

--- Assets/Scripts/master_control.cs
+++ Assets/Scripts/master_control.cs
@@ -14,9 +14,15 @@ public class master_control : MonoBehaviour
     void Update()
     {
         // User pressing left arrow key will rotate the master object left
-        // if (Input.GetKey(KeyCode.RightArrow))
-        // {
-        //     transform.rotation = Quaternion.Euler(0, 0, 1 * Time.deltaTime);
-        // }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            // move the master object angle by one degree
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z + 1);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            // move the master object angle by one degree
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z - 1);
+        }
     }
 }
